a49c6fc848bdcc50073710194ac2a73b5e60e511
muherezaamon <51474818+muherezaamon@users.noreply.github.com>
2019-06-14 05:44:44 -0400

    null checker

---
+++ null_cheaker.js
@@ -0,0 +1,18 @@
+for(j=1; j<=10; j++)//for loop to iterate the count 10 times
+    {
+        
+        let counter = prompt('Enter number:');//promts to enter number
+     if (counter>0)
+    
+          {
+           console.log(1); //outputs number is greater than zero
+          }
+       else if (counter<0)
+          {
+               console.log(-1); //outputs number if less thn zero
+          }
+    
+        else
+        {
+          console.log(0);// outputs a zero if number is zero
+        }
